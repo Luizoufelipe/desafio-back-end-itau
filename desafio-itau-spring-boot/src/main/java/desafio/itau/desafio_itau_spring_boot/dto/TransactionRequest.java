ca4c53c764268bffc30e5b6498cf90ed272cad90
Luizoufelipe <felipe2003zuckado@gmail.com>
2026-03-09 01:04:28 -0300

    fix: corrige endpoints de transacao e estatistica

--- desafio-itau-spring-boot/src/main/java/desafio/itau/desafio_itau_spring_boot/dto/TransactionRequest.java
+++ desafio-itau-spring-boot/src/main/java/desafio/itau/desafio_itau_spring_boot/dto/TransactionRequest.java
@@ -1,11 +1,21 @@
 package desafio.itau.desafio_itau_spring_boot.dto;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.OffsetDateTime;
 
 public class TransactionRequest {
 
-    private double Valor;
+    @NotNull
+    private double valor;
 
+    @NotNull
     private OffsetDateTime dataHora;
 
+    public double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
 }
